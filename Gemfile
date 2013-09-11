2bed438b47d41d7ab4c25b2018db8a6b74cb601e
Alex Slynko <alex.slynko@wonga.com>
2013-09-11 15:36:07 +0100

    TD-1110: Save billing data in database

--- Gemfile
+++ Gemfile
@@ -2,7 +2,7 @@ source 'https://rubygems.org'
 
 gem 'daemons'
 gem 'aws-sdk'
-gem 'pantry_daemon_common', git: 'git@github.com:wongatech/pantry_daemon_common.git', :tag => <insert version here>
+gem 'pantry_daemon_common', git: 'git@github.com:wongatech/pantry_daemon_common.git', :tag => 'v0.1.6'
 gem 'rest-client'
 
 group :development do
--- daemon.rb
+++ daemon.rb
@@ -5,4 +5,4 @@ require_relative 'bill_imported_event_handler/bill_imported_event_handler'
 
 config_name = File.join(File.dirname(File.symlink?(__FILE__) ? File.readlink(__FILE__) : __FILE__), "config", "daemon.yml")
 Wonga::Daemon.load_config(File.expand_path(config_name))
-Wonga::Daemon.run(Wonga::Daemon::BillImportedEventHandler.new(Wonga::Daemon.publisher,Wonga::Daemon.logger))
+Wonga::Daemon.run(Wonga::Daemon::BillImportedEventHandler.new(Wonga::Daemon.pantry_api_client, Wonga::Daemon.logger))
