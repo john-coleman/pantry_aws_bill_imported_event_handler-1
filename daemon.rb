2fc874f15023a645a8f4453667ff3088fd5de6d2
John Coleman <john.coleman@wonga.com>
2015-06-05 19:19:06 +0100

    RM-1764 pantry_daemon_common memory inflation
    * Don't require 'pry' :)
    
    Change-Id: Iacf8d4bfa5c1e4aa8346aa4840092634876d6628

--- daemon.rb
+++ daemon.rb
@@ -2,7 +2,6 @@
 require 'rubygems'
 require 'wonga/daemon'
 require_relative 'bill_imported_event_handler/bill_imported_event_handler'
-require 'pry'
 
 config_name = File.join(File.dirname(File.symlink?(__FILE__) ? File.readlink(__FILE__) : __FILE__), 'config', 'daemon.yml')
 Wonga::Daemon.load_config(File.expand_path(config_name))
