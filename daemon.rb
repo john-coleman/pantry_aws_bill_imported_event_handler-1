3359b0d7bc2e0cb85fe24a065cbcfec8edd086aa
John Coleman <john.coleman@wonga.com>
2015-06-05 18:31:43 +0100

    RM-1764 pantry_daemon_common memory inflation
    * Upgraded to pantry_daemon_common v0.5.6
    
    Change-Id: Icb49076c125766fc2c2e0ded3a0c3a6bc00bcf00

--- daemon.rb
+++ daemon.rb
@@ -2,7 +2,8 @@
 require 'rubygems'
 require 'wonga/daemon'
 require_relative 'bill_imported_event_handler/bill_imported_event_handler'
+require 'pry'
 
 config_name = File.join(File.dirname(File.symlink?(__FILE__) ? File.readlink(__FILE__) : __FILE__), 'config', 'daemon.yml')
 Wonga::Daemon.load_config(File.expand_path(config_name))
-Wonga::Daemon.run(Wonga::Daemon::BillImportedEventHandler.new(Wonga::Daemon.pantry_api_client, Wonga::Daemon.logger))
+Wonga::Daemon.run(Wonga::Daemon::BillImportedEventHandler.new(Wonga::Daemon.pantry_api_client, Wonga::Daemon.config))
